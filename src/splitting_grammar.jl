39e40e63368a8d1b587bb8b1590705b7b7a3b640
Dennis Heijmans <D.Heijmans@student.tudelft.nl>
2024-05-17 20:11:38 +0200

    Implement part of splitting algorithm

--- src/splitting_grammar.jl
+++ src/splitting_grammar.jl
@@ -1,4 +1,5 @@
 struct Edge
+    symbol::Symbol
     dependencies::Set{Symbol}
     rules::Vector{Expr}
 end
@@ -9,15 +10,46 @@ end
 
 struct Graph
     nodes::Dict{Symbol,Node}
+    edges::Vector{Edge}
 end
 
 function split_grammar(grammar::ContextSensitiveGrammar, start::Symbol)::Vector{ContextSensitiveGrammar}
     # Build a graph representing the dependencies between the rules
     graph = build_graph(grammar)
 
-    # TODO: Implement the splitting algorithm
+    # For each rule, find the smallest sub-grammar that contains it
+    paths = []
+    for edge in graph.edges
+        edges = Set{Edge}([edge])
+        union!(edges, find_path(graph, start, edge.symbol))
+        for dependency in edge.dependencies
+            union!(edges, find_path(graph, dependency, nothing))
+        end
+        push!(paths, edges)
+    end
 
-    return []
+    # TODO: Remove subsets
+
+    # Create a sub-grammar from each path
+    subgrammars = []
+    for path in paths
+        g = @cfgrammar begin end
+        for edge in path
+            for rule in edge.rules
+                g = add_rule!(deepcopy(g), rule)
+            end
+        end
+        push!(subgrammars, g)
+    end
+
+    return subgrammars
+end
+
+function find_path(graph::Graph, start::Symbol, target::Union{Symbol,Nothing})::Set{Edge}
+    
+    # TODO: Find the shortest path from start to target
+
+    return Set{Edge}()
 end
 
 function build_graph(grammar::ContextSensitiveGrammar)::Graph
@@ -31,9 +63,10 @@ function build_graph(grammar::ContextSensitiveGrammar)::Graph
     nodes = Dict{Symbol, Node}()
     for symbol in symbols
         nodes[symbol] = Node([])
-    end     
+    end
 
     # Place every rule on an edge with the correct dependencies
+    edges = []
     for i in 1:length(grammar.rules)
         rule = :($(grammar.types[i]) = $(grammar.rules[i]))
         dependencies = Set{Symbol}(grammar.childtypes[i])
@@ -50,9 +83,11 @@ function build_graph(grammar::ContextSensitiveGrammar)::Graph
 
         # Create a new edge if no match was found
         if !matched
-            push!(nodes[grammar.types[i]].edges, Edge(dependencies, [rule]))        
+            edge = Edge(grammar.types[i], dependencies, [rule])
+            push!(edges, edge)
+            push!(nodes[grammar.types[i]].edges, edge)        
         end
     end
     
-    return Graph(nodes)
+    return Graph(nodes, edges)
 end
